5a13bd7db46290878cd84afb6052f96dfade5b05
Martin Eduardo Aguayo <martineduardoaguayo@gmail.com>
2024-04-30 12:37:04 -0300

    Initial commit

---
+++ src/components/sidebar-responsive.tsx
@@ -0,0 +1,11 @@
+export const SidebarReponsive = () => {
+  return (
+    <ul className="absolute right-0 left-0 bottom-0 top-32 text-center space-y-8 ">
+      <li>Work</li>
+      <li>Skills</li>
+      <li>About</li>
+      <li>CV</li>
+      <li>Contact</li>
+    </ul>
+  );
+};
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+export const Sidebar = () => {
+  return (
+    <ul className="flex flex-col gap-y-16 pt-16 pl-8 ">
+      <li>Work</li>
+      <li>Skills</li>
+      <li>About</li>
+      <li>CV</li>
+      <li>Contact</li>
+    </ul>
+  );
+};
